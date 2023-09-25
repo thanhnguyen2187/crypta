4015e578332dbad501af2647340b0908fcd750f4
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-09-26 02:13:37 +0700

    Create the first layout WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div
+    class="container m-4 flex flex-row gap-4"
+>
+    <div class="flex flex-col w-40 border-2 gap-2">
+        <div class="p-2">Title</div>
+        <div>Text</div>
+    </div>
+    <div class="flex flex-col w-40 border-2">
+        <div>Title</div>
+        <div>Text</div>
+    </div>
+    <div class="flex flex-col w-40 border-2">
+        <div>Title</div>
+        <div>Text</div>
+    </div>
+    <div class="flex flex-col w-40 border-2">
+        <div>Title</div>
+        <div>Text</div>
+    </div>
+</div>
