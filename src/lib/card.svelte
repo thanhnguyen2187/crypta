15a9dec0a1db6b03177fafb34b24659c0eec2420
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-09-28 04:49:36 +0700

    Implement card component

---
+++ src/lib/card.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import IconExpandIon from '../svg/icon-expand-ion-24.svelte'
+  import IconLockIon from '../svg/icon-lock-ion-24.svelte'
+  import IconTrashIon from '../svg/icon-trash-ion-24.svelte'
+
+  export let title = 'Sample title'
+  export let content = 'Sample content'
+  export let invisible = false
+</script>
+
+<div
+  class="flex flex-col w-80 border-2 rounded-2xl {invisible ? 'invisible' : ''}"
+>
+  <div
+    class="flex justify-between"
+  >
+    <input class="m-2 px-1 border-2 rounded-xl" value="{title}"/>
+    <div class="mr-3 my-3 gap-1 flex">
+      <IconExpandIon/>
+      <IconLockIon/>
+      <IconTrashIon/>
+    </div>
+  </div>
+  <div
+    class="flex flex-col relative"
+  >
+    <div
+      class="absolute right-0 mt-2 mr-2 px-2 border-2 rounded-l"
+    >
+      Clojure
+    </div>
+    <textarea rows="5" class="m-2 border-2 px-1">{content}</textarea>
+  </div>
+  <div
+    class="flex justify-between m-2"
+  >
+    <div>
+      #tag-1 #tag-2
+    </div>
+    <div>
+      Another
+    </div>
+  </div>
+</div>
---
+++ src/svg/icon-expand-ion-24.svelte
@@ -0,0 +1,3 @@
+<svg width="24" height="24" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+  <path fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M432 320v112H320m101.8-10.23L304 304M80 192V80h112M90.2 90.23L208 208M320 80h112v112M421.77 90.2L304 208M192 432H80V320m10.23 101.8L208 304"/>
+</svg>
