db1804ab32dd547fbea4ee375b64f6f6f0a78086
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-10-02 01:03:36 +0700

    Change Toaster direction

--- src/lib/toaster.svelte
+++ src/lib/toaster.svelte
@@ -6,6 +6,6 @@
 {#if $toasterText}
   <div
     class="absolute top-2 right-4 bg-white border-2 rounded-xl py-1 px-2"
-    transition:fly={{ x: 40, y: 0 }}
+    transition:fly={{ x: 0, y: -50 }}
   >{$toasterText}</div>
 {/if}
