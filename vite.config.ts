2df808b2124bea0e6b2dc438cd98e3dba923c1dd
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-10-04 22:53:34 +0700

    Add `vite-plugin-node-polyfills`

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), nodePolyfills()],
+	optimizeDeps: {
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
