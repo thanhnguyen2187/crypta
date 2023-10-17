ec3db8e4f380525903e033650d74f314f5fbcc11
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-10-17 23:41:47 +0700

    Add button for settings

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,5 +1,13 @@
+<script
+  lang="ts"
+>
+  import IconSettings from '../svg/icon-settings-ion-48.svelte'
+
+  let hovered = false
+</script>
+
 <div
-  class="py-4 px-8 flex justify-between"
+  class="flex items-center justify-between"
 >
   <div></div>
   <div
@@ -10,5 +18,17 @@
       placeholder="search here"
     />
   </div>
-  <div>Right</div>
+  <div
+    on:mouseenter={() => hovered = true}
+    class="w-16 h-16 flex items-center justify-center"
+    on:mouseleave={() => hovered = false}
+    class:bg-gray-200={hovered}
+  >
+    <div
+      class="opacity-25"
+      class:opacity-50={hovered}
+    >
+      <IconSettings/>
+    </div>
+  </div>
 </div>
