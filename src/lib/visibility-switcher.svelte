d7c981e4b6f7aac970132dc12d0a449d7f6a9695
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-10-19 00:14:46 +0700

    Improve `VisibilitySwitcher` clicking visual

--- src/lib/visibility-switcher.svelte
+++ src/lib/visibility-switcher.svelte
@@ -3,20 +3,28 @@
   import IconArrowBack from '../svg/icon-ion-arrow-back-64.svelte'
   import { sectionState, nextSectionState, setSectionState } from './store'
 
+  let state : 'default' | 'hovered' | 'clicked' = 'default'
   let hovered = false
+  let clicked = false
+  $: hovered = state === 'hovered'
+  $: clicked = state === 'clicked'
 </script>
 
 <div
   class="h-screen w-16 transition-opacity flex items-center"
   class:bg-gray-200={hovered}
-  on:mouseenter={() => hovered = true}
+  class:bg-gray-300={clicked}
+  on:mouseenter={() => state = 'hovered'}
+  on:mousedown={() => state = 'clicked'}
   on:click={() => setSectionState(nextSectionState($sectionState))}
-  on:mouseleave={() => hovered = false}
+  on:mouseup={() => state = 'default'}
+  on:mouseleave={() => state = 'default'}
 >
   <div
     class="opacity-25 my-auto"
     style="width: 64px; height: 64px;"
     class:opacity-50={hovered}
+    class:opacity-75={clicked}
   >
     {#if $sectionState === 'public'}
       <IconArrowForward/>
