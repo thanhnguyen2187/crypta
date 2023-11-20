02d25980ed6ec2f658da0147a0421c9b55a0bfac
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-11-20 20:38:51 +0700

    Draft `createRemoteSnippetStore`

--- src/lib/utitlities/synchronization.ts
+++ src/lib/utitlities/synchronization.ts
@@ -1,3 +1,6 @@
+import { derived, get, writable } from 'svelte/store'
+import { dialogSettingsStateStore, dialogSettingsStore, dialogStateStore } from '$lib/components/dialog/dialog'
+
 export type SyncState = 'conflicted' | 'synchronized' | 'localOnly'
 export type DataState = {
   [id: string]: {
@@ -5,4 +8,23 @@ export type DataState = {
     remoteRecord: {}
     syncState: SyncState
   }
+}
+
+export async function createRemoteSnippetStore() {
+  const settings = get(dialogSettingsStore)
+  const endpoint = `${settings.serverURL}/api/v1/snippets`
+
+  const store = derived(
+    dialogSettingsStateStore,
+    async (settingsStatePromise) => {
+      const settingsState = await settingsStatePromise
+      if (settingsState !== 'connected') {
+        return []
+      }
+
+      return await fetch(endpoint)
+    },
+  )
+
+  return store
 }
