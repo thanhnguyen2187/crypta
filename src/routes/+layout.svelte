d8bc9b1c26a692f56bed33d891e443ea2542ec22
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-12-02 17:21:27 +0700

    Draft barebone MVP

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
   import "../app.postcss";
+  import { initializeStores } from '@skeletonlabs/skeleton';
+  import '@fortawesome/fontawesome-free/css/fontawesome.css';
+  import '@fortawesome/fontawesome-free/css/brands.css';
+  import '@fortawesome/fontawesome-free/css/solid.css';
+
+  initializeStores();
 </script>
 
 <slot />
