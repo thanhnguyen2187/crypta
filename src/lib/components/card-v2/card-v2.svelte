86c963d75f9492f4cc706f2e9d9d6b094ac3caa8
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-12-04 16:19:58 +0700

    Making `CardV2` works

--- src/lib/components/card-v2/card-v2.svelte
+++ src/lib/components/card-v2/card-v2.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import { CodeBlock, popup } from '@skeletonlabs/skeleton'
-  import type { Snippet } from '$lib/utitlities/persistence';
+  import type { Snippet } from '$lib/utitlities/persistence'
+  import LockIcon from './lock-icon.svelte'
+  import { localSnippetStore } from '$lib/components/card/card';
 
+  let lockMouseState = 'none' | 'hover' | 'active'
   export let snippet: Snippet = {
     id: '',
     name: '',
@@ -12,6 +15,44 @@
     createdAt: 0,
     updatedAt: 0,
   }
+
+  type CardAction = {
+    text: string
+    faIconClass: string
+    callback: () => void
+  }
+  const cardActions: CardAction[] = [
+    {
+      text: 'Lock',
+      faIconClass: 'fa-lock',
+      callback: () => {},
+    },
+    {
+      text: 'Duplicate',
+      faIconClass: 'fa-copy',
+      callback: () => localSnippetStore.clone(snippet),
+    },
+    {
+      text: 'Edit',
+      faIconClass: 'fa-edit',
+      callback: () => {},
+    },
+    {
+      text: 'Delete',
+      faIconClass: 'fa-trash',
+      callback: () => {},
+    },
+    {
+      text: 'Import',
+      faIconClass: 'fa-file-import',
+      callback: () => {},
+    },
+    {
+      text: 'Export',
+      faIconClass: 'fa-file-export',
+      callback: () => {},
+    },
+  ]
 </script>
 
 <div
@@ -31,42 +72,36 @@
     >
       <i class="fa-xl fa-solid fa-ellipsis-v"></i>
     </button>
-    <div data-popup="card-actions-{snippet.id}">
+    <div
+      data-popup="card-actions-{snippet.id}"
+      class="z-10"
+    >
+      <!--Use `z-10` to make it displays above AppShell-->
       <ul class="list-nav variant-filled gap-2 rounded-container-token">
-        <li>
-          <button class="w-full">
+        {#each cardActions as cardAction}
+          <li>
+            <button class="w-full">
             <span>
-              <i class="fa-solid fa-edit"></i>
+              <i class="fa-solid {cardAction.faIconClass}"></i>
             </span>
-            <span>Edit</span>
-          </button>
-        </li>
-        <li>
-          <button class="w-full">
-            <span>
-              <i class="fa-solid fa-copy"></i>
-            </span>
-            <span>Duplicate</span>
-          </button>
-        </li>
-        <li>
-          <button class="w-full">
-            <span>
-              <i class="fa-solid fa-trash"></i>
-            </span>
-            <span>Delete</span>
-          </button>
-        </li>
+              <span>{cardAction.text}</span>
+            </button>
+          </li>
+        {/each}
       </ul>
     </div>
   </header>
   <section
     class="m-4 h-40 overflow-y-scroll hide-scrollbar"
   >
-    <CodeBlock
-      language={snippet.language}
-      code={snippet.text}
-    />
+    {#if !snippet.encrypted}
+      <CodeBlock
+        language={snippet.language}
+        code={snippet.text}
+      />
+    {:else}
+      <LockIcon />
+    {/if}
   </section>
   <footer
     class="card-footer"
