127b84086fac593b6039c716c071b9d2620dc6f7
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-12-13 11:20:04 +0700

    Make build static site works

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vitest/config'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
-	plugins: [sveltekit(), nodePolyfills()],
+	plugins: [sveltekit(), nodePolyfills(), topLevelAwait()],
 	optimizeDeps: {
 	},
 	test: {
