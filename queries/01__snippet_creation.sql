4c67f0f185c931c7ca3d7f9b159d2581c39fa687
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-12-18 19:51:45 +0700

    Make snippet creation works WIP

--- queries/01__snippet_creation.sql
+++ queries/01__snippet_creation.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS snippets (
     id TEXT PRIMARY KEY,
-    'name' TEXT,
-    'language' TEXT,
-    'text' TEXT,
-    'encrypted' BOOLEAN,
+    "name" TEXT,
+    "language" TEXT,
+    "text" TEXT,
+    "encrypted" BOOLEAN,
     position REAL,
     updated_at DATETIME,
     created_at DATETIME
--- src/lib/sqlite/browser.ts
+++ src/lib/sqlite/browser.ts
@@ -2,21 +2,33 @@
 import SQLiteESMFactory from '/wa-sqlite.mjs?url'
 import * as SQLite from 'wa-sqlite'
 // @ts-ignore
-import { IDBBatchAtomicVFS } from 'wa-sqlite/src/examples/IDBBatchAtomicVFS.js'
+// import * as IDBBatchAtomicModule from 'wa-sqlite/src/examples/IDBBatchAtomicVFS.js'
+import * as IDBBatchAtomicModule from 'wa-sqlite/src/examples/IDBBatchAtomicVFS.js'
 
 export async function hello() {
   // @ts-ignore
   const module = await SQLiteESMFactory()
   const sqlite3 = SQLite.Factory(module)
-  sqlite3.vfs_register(IDBBatchAtomicVFS)
+  const vfs = new IDBBatchAtomicModule['IDBBatchAtomicVFS']([])
+  await vfs.isReady
+  sqlite3.vfs_register(vfs, true)
   const db = await sqlite3.open_v2('mydb')
   const queryContent = (await import('../../../queries/01__snippet_creation.sql?raw')).default
+  debugger
   await sqlite3.exec(
     db,
+    // `SELECT 'Hello world'`,
     queryContent,
-    (row, columns) => {
-      console.log(row)
-    }
+    // (row, columns) => {
+    //   console.log(row)
+    // }
   )
+  // await sqlite3.exec(
+  //   db,
+  //   `SELECT * FROM snippets;`,
+  //   (row, columns) => {
+  //     console.log(row)
+  //   }
+  // )
   await sqlite3.close(db);
 }
