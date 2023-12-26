be640bc744d83aefe8e4bfa44f355ad30883a708
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-12-26 12:45:55 +0700

    Move SQL queries out of static serving

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,5 +3,5 @@ import { Config } from 'drizzle-kit'
 export default {
   schema: './src/lib/sqlite/schema.ts',
   driver: 'better-sqlite',
-  out: './static/queries',
+  out: './db',
 } satisfies Config
