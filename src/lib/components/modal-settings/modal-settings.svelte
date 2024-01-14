f54d96edafda5045d939f3cf0c29d116ee480abe
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-01-14 23:25:27 +0700

    Make settings persistence works

--- src/lib/components/modal-settings/modal-settings.svelte
+++ src/lib/components/modal-settings/modal-settings.svelte
@@ -1,3 +1,54 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import { settingsStore } from '$lib/utitlities/ephemera'
+</script>
 
-<div>Hello world</div>
+<div
+  class="card w-modal-slim relative"
+>
+  <button
+    class="badge-icon absolute md:hidden top-2 right-2"
+  >
+    <i class="fa-xl fa-solid fa-close"></i>
+  </button>
+  <section class="m-4 flex flex-col gap-2">
+    <label class="label">
+      <span>Server URL</span>
+      <input
+        class="input"
+        spellcheck="false"
+        bind:value={$settingsStore.serverURL}
+      />
+    </label>
+    <label class="label">
+      <span>Username</span>
+      <input
+        class="input"
+        spellcheck="false"
+        bind:value={$settingsStore.username}
+      />
+    </label>
+    <label class="label">
+      <span>Password</span>
+      <input
+        class="input"
+        type="password"
+        spellcheck="false"
+        bind:value={$settingsStore.password}
+      />
+    </label>
+    <label class="label">
+      <span>Logs</span>
+      <textarea
+        class="textarea font-mono"
+        readonly="readonly"
+        rows="7"
+      >- No server URL detected</textarea>
+    </label>
+  </section>
+  <footer class="card-footer flex gap-2 justify-end">
+    <button class="btn variant-filled">
+      <i class="fa-solid fa-trash"></i>
+      <span>Clear Logs</span>
+    </button>
+  </footer>
+</div>
