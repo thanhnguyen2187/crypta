f3ae3acfc9c9cfda76167e8b75bf20a4daf1fdfd
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-02-06 02:03:27 +0700

    Refactor testing code

--- src/lib/sqlite/sqliterg.test.ts
+++ src/lib/sqlite/sqliterg.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, expectTypeOf, assertType } from 'vitest'
-import { createRemoteDb, createRemoteSnippetStore, createSqlitergExecutor } from './sqliterg'
+import { createRemoteDb, createRemoteSnippetStore, createSqlitergExecutor, SqlitergExecutor } from './sqliterg'
 import type { ResponseExecuteError } from './sqliterg'
 import { migrate, defaultMigrationQueryMap, defaultQueriesStringMap } from './migration'
 import type { MigrationState } from './migration'
@@ -10,45 +10,57 @@ import type { Settings } from '$lib/utitlities/ephemera'
 import type { GlobalState } from '$lib/utitlities/persistence';
 import { createNewSnippet } from '$lib/utitlities/persistence'
 
-const TestServerURL = 'http://127.0.0.1:12322/crypta'
-const TestServerURLWrong = 'http://127.0.0.1:12323/crypta'
-const TestServerUsername = 'crypta'
-const TestServerPassword = 'crypta'
+enum Constants {
+  ServerURL = 'http://127.0.0.1:12321/crypta',
+  ServerURLWrong = 'http://127.0.0.1:12322/crypta',
+  Blank = '',
+  Username = 'crypta',
+  Password = 'crypta',
+  PasswordWrong = 'wrong'
+}
+
+function createUnreachableExecutor(): SqlitergExecutor {
+  return createSqlitergExecutor(
+    Constants.ServerURLWrong,
+    Constants.Blank,
+    Constants.Blank,
+  )
+}
+
+function createUnauthenticatedExecutor(): SqlitergExecutor {
+  return createSqlitergExecutor(
+    Constants.ServerURL,
+    Constants.Username,
+    Constants.PasswordWrong,
+  )
+}
+
+function createAvailableExecutor(): SqlitergExecutor {
+  return createSqlitergExecutor(
+    Constants.ServerURL,
+    Constants.Username,
+    Constants.Password,
+  )
+}
 
 // IMPORTANT: `yarn dev-db` should be run before the tests are executed
 describe('executor', () => {
   it('unreachable', async () => {
-    const executor = createSqlitergExecutor(
-      TestServerURLWrong,
-      '',
-      '',
-    )
+    const executor = createUnreachableExecutor()
     expect(await executor.isReachable()).toBe(false)
   })
-  it('reachable but unauthorized', async () => {
-    const executor = createSqlitergExecutor(
-      'http://127.0.0.1:12321/crypta',
-      '',
-      '',
-    )
+  it('reachable but unauthenticated', async () => {
+    const executor = createUnauthenticatedExecutor()
     expect(await executor.isReachable()).toBe(true)
     expect(await executor.isAuthenticated()).toBe(false)
   })
-  it('reachable and authorized', async () => {
-    const executor = createSqlitergExecutor(
-      'http://127.0.0.1:12321/crypta',
-      'crypta',
-      'crypta',
-    )
+  it('reachable and authenticated', async () => {
+    const executor = createAvailableExecutor()
     expect(await executor.isReachable()).toBe(true)
     expect(await executor.isAuthenticated()).toBe(true)
   })
   it('basic query', async () => {
-    const executor = createSqlitergExecutor(
-      'http://127.0.0.1:12321/crypta',
-      'crypta',
-      'crypta',
-    )
+    const executor = createAvailableExecutor()
     {
       const result = await executor.execute('SELECT 1', [])
       expectTypeOf(result).not.toEqualTypeOf<ResponseExecuteError>()
@@ -69,11 +81,7 @@ describe('executor', () => {
 
 describe('remote database', () => {
   it('basic query', async () => {
-    const executor = createSqlitergExecutor(
-      'http://127.0.0.1:12321/crypta',
-      'crypta',
-      'crypta',
-    )
+    const executor = createAvailableExecutor()
     const remoteDb = createRemoteDb(executor)
     {
       const result = await remoteDb.get(sql`SELECT 1`)
@@ -81,11 +89,7 @@ describe('remote database', () => {
     }
   })
   it('migration', async () => {
-    const executor = createSqlitergExecutor(
-      'http://127.0.0.1:12321/crypta',
-      'crypta',
-      'crypta',
-    )
+    const executor = createAvailableExecutor()
     const remoteDb = createRemoteDb(executor)
     const dummyStateStore = writable<MigrationState>('not-started')
     const dummyDataImportFn = async () => {}
@@ -124,71 +128,54 @@ describe('remote database', () => {
 })
 
 describe('remote snippet store', async () => {
-  const dummyStateStore = writable<MigrationState>('done')
-  const dummySettingsStore = writable<Settings>({
-    serverURL: 'http://127.0.0.1:12322/crypta',
-    username: '',
-    password: '',
-  })
-  const dummyGlobalStore = writable<GlobalState>({
-    folderId: 'default',
-    tags: [],
-    searchInput: '',
-  })
-  const dummyExecutorStore = derived(
-    dummySettingsStore,
-    (settings) => createSqlitergExecutor(
-      settings.serverURL,
-      settings.username,
-      settings.password,
-    )
-  )
-  const remoteSnippetStore = await createRemoteSnippetStore(
-    dummyStateStore,
-    dummyGlobalStore,
-    dummyExecutorStore,
-  )
-
   it('availability', async () => {
-    // unreachable server
-    dummySettingsStore.set({
-      serverURL: 'http://127.0.0.1:12322/crypta',
-      username: '',
-      password: '',
+    const dummyStateStore = writable<MigrationState>('done')
+    const dummyGlobalStore = writable<GlobalState>({
+      folderId: 'default',
+      tags: [],
+      searchInput: '',
     })
+    const dummyExecutorStore = writable<SqlitergExecutor>(createAvailableExecutor())
+    const remoteSnippetStore = await createRemoteSnippetStore(
+      dummyStateStore,
+      dummyGlobalStore,
+      dummyExecutorStore,
+    )
+
+    // unreachable server
     {
+      dummyExecutorStore.set(createUnreachableExecutor())
       const availability = await remoteSnippetStore.isAvailable()
       expect(availability).toBe(false)
     }
 
     // reachable server with wrong authentication
-    dummySettingsStore.set({
-      serverURL: 'http://127.0.0.1:12321/crypta',
-      username: 'crypta',
-      password: 'wrong password',
-    })
     {
+      dummyExecutorStore.set(createUnauthenticatedExecutor())
       const availability = await remoteSnippetStore.isAvailable()
       expect(availability).toBe(false)
     }
 
     // available server
-    dummySettingsStore.set({
-      serverURL: 'http://127.0.0.1:12321/crypta',
-      username: 'crypta',
-      password: 'crypta',
-    })
     {
+      dummyExecutorStore.set(createAvailableExecutor())
       const availability = await remoteSnippetStore.isAvailable()
       expect(availability).toBe(true)
     }
   })
   it('crud', async () => {
-    dummySettingsStore.set({
-      serverURL: 'http://127.0.0.1:12321/crypta',
-      username: 'crypta',
-      password: 'crypta',
+    const dummyStateStore = writable<MigrationState>('done')
+    const dummyGlobalStore = writable<GlobalState>({
+      folderId: 'default',
+      tags: [],
+      searchInput: '',
     })
+    const dummyExecutorStore = writable<SqlitergExecutor>(createAvailableExecutor())
+    const remoteSnippetStore = await createRemoteSnippetStore(
+      dummyStateStore,
+      dummyGlobalStore,
+      dummyExecutorStore,
+    )
 
     const availability = await remoteSnippetStore.isAvailable()
     expect(availability).toBe(true)
